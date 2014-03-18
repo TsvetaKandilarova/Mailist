330f7d98207d7ad82c790fbe028ff3a860d77b6a
pepi09 <pepa.p.simeonova@abv.bg>
2014-03-18 21:45:02 +0200

    Fixed add function --checking for existing emails

--- add.py
+++ add.py
@@ -14,16 +14,35 @@ def add():
             email = input("Email ")
             contact.append(name)
             contact.append(email)
-            list.write("\n%s - %s" %(contact[0],contact[1]))
-            print("%s <%s> was added to %s!" %(name,email,name_list))
+            _email = open("%s" %(_list[k]),"r")
+            contents = _email.read()
+            if not contains_substring(contents,email):
+                list.write("\n%s - %s" %(contact[0],contact[1]))
+                print("%s <%s> was added to %s!" %(name,email,name_list))
+            else:
+                print("A person with the given email %s is already in the list!" %(email))
         else:
             print("List with unique identifier %s was not found!" %(sys.argv[1]))
     else:
         print("Wrong input!")
 
+def contains_substring(haystack, needle):
+    i = 0
+    while i < len(haystack):
+        if haystack[i] == needle[0]:
+            k = 0
+            while  k < len(needle) and i < len(haystack) and haystack[i] == needle[k]:
+                i = i+1
+                k = k+1
+            if k == len(needle):
+                return True
+            i = i - 1
+        i = i + 1
+    return False
 
 def main():
     add()
+    #print(contains_substring("haystack", "yst"))
 
 if __name__ == '__main__':
     main()
