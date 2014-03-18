c54bd9f35572c70855c62d3217f5cf1a236d524e
pepi09 <pepa.p.simeonova@abv.bg>
2014-03-18 19:47:17 +0200

    Adding help.py and help.txt

--- mail.py
+++ mail.py
@@ -2,8 +2,8 @@ from subprocess import call
 def main():
     print("Hello Stranger! This is a cutting-edge, console-based mail-list!Type help, to see a list of commands.")
     while True:
-        command = input("")
-        call("py %s" %(command))
+        command = input("Enter command ")
+        call("py %s.py" %(command),shell = True)
 
 
 
